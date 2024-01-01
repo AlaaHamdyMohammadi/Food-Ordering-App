673f9c91840a300394e884873930acc27064ffa8
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-02 01:28:01 +0200

    Building Homepage

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -3,27 +3,27 @@ import Right from "../Icons/Right";
 
 function Hero() {
   return (
-    <section className="grid grid-cols-2 my-20">
-      <div className="">
+    <section className="hero mt-20">
+      <div>
         <h1 className="text-4xl font-semibold">
-          Everything is better with a Pizza
+          Everything is better with a <span className="text-primary">Pizza</span>
         </h1>
-        <p className="my-4 text-gray-500">
+        <p className="my-6 text-gray-500 text-sm">
           Pizza is the missing piece that makes every day complete, a simple yet
           delicious join in life
         </p>
-        <div className="flex items-center gap-4">
-          <button className="bg-primary flex gap-2 text-white px-4 py-2 rounded-full">
+        <div className="flex items-center gap-4 text-sm">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
             Order now
             <Right />
           </button>
-          <button className="flex gap-2 text-gray-600 font-semibold">
-            Learn more 
+          <button className="flex items-center gap-2 text-gray-600 font-semibold">
+            Learn more
             <Right />
           </button>
         </div>
       </div>
-      <div className="relative">
+      <div className="relative left-16">
         <Image
           src={"/pizza.png"}
           alt={"Pizza"}
