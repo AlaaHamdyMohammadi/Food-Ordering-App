27da783ff397a0b39c02a35a2bc2658776dcd9af
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-03 23:19:44 +0200

    Authentication

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
+
 function RegisterPage() {
-    return (
-      <section>
-        <h1 className="text-center text-primary text-4xl"> Register</h1>
-        <form>
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <button type="submit">Register</button>
-            </form>  
-      </section>
-    );
+  return (
+    <section>
+      <h1 className="text-center text-primary text-4xl"> Register</h1>
+      <form className="block max-w-xs mx-auto">
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Register</button>
+        <div className="my-4 text-center text-gray-500">Or login with provider</div>
+        <button className="flex items-center justify-center gap-4">
+        <Image src={"/google.png"} alt={"google"} width={24} height={24} />
+        Login with google
+        </button>
+      </form>
+    </section>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
