a98a442993c5d67ea06707a7d7c817e5855516e1
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-06 23:27:06 +0200

    Handle Register

---
+++ src/app/api/register/route.js
@@ -0,0 +1,6 @@
+export function handler(req, res){
+    if(req.method === 'POST'){
+        const {email, password} = req.body;
+    }
+
+}
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,17 +1,46 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('/api/register', {
+        method: 'POST',
+        body: JSON.stringify({email, password}),
+        headers: {
+            'Conten-Type': 'application/json',
+        }
+    })
+  }
+
   return (
     <section>
       <h1 className="text-center text-primary text-4xl"> Register</h1>
-      <form className="block max-w-xs mx-auto">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form className="block max-w-xs mx-auto" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Register</button>
-        <div className="my-4 text-center text-gray-500">Or login with provider</div>
+        <div className="my-4 text-center text-gray-500">
+          Or login with provider
+        </div>
         <button className="flex items-center justify-center gap-4">
-        <Image src={"/google.png"} alt={"google"} width={24} height={24} />
-        Login with google
+          <Image src={"/google.png"} alt={"google"} width={24} height={24} />
+          Login with google
         </button>
       </form>
     </section>
