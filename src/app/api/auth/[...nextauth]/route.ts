3d00526032fc71c58ee2e1a30dcc0b3760de8582
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-08 23:30:51 +0200

    Update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,9 +15,7 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
         
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
+        
         const res = await fetch("/your/endpoint", {
           method: "POST",
           body: JSON.stringify(credentials),
