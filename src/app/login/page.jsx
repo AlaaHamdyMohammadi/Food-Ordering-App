3f3fb6ef86c28cb6a2c66444aa51027617e0747a
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-11 00:09:06 +0200

    Add Login Page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,32 @@
+"use client";
+import { useState } from "react";
+
 function LoginPage() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+      <form className="block max-w-xs mx-auto" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={creatingUser}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={creatingUser}
+        />
+      </form>
+    </section>
+  );
 }
 
 export default LoginPage;
