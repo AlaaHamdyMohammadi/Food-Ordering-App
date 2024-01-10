caa10168f34b293f723fc97ed6fe30f83ca59803
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-11 00:18:31 +0200

    Update

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
 
 function LoginPage() {
@@ -9,7 +10,7 @@ function LoginPage() {
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
-      <form className="block max-w-xs mx-auto">
+      <form className="block max-w-xs mx-auto ">
         <input
           type="email"
           placeholder="email"
@@ -24,6 +25,14 @@ function LoginPage() {
           onChange={(e) => setPassword(e.target.value)}
           disabled={creatingUser}
         />
+        <button type="submit">Login</button>
+        <div className="my-4 text-center text-gray-500">
+          Or login with provider
+        </div>
+        <button className="flex items-center justify-center gap-4">
+          <Image src={"/google.png"} alt={"google"} width={24} height={24} />
+          Login with google
+        </button>
       </form>
     </section>
   );
