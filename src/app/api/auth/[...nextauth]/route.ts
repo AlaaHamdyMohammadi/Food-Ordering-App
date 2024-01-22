a87e98e839abf8c1e8bdf5697f20f806386d0d1e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-22 23:51:24 +0200

    Update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,9 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
         
         const res = await fetch("/your/endpoint", {
           method: "POST",
