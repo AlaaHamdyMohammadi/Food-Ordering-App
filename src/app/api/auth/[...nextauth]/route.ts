d20abf8c144baec5c7ddaa1a5caf164622a1e108
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-23 23:44:57 +0200

    Update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,7 @@ const handler = NextAuth({
         });
         const user = await res.json();
 
-        // If no error and we have user data, return it
+        
         if (res.ok && user) {
           return user;
         }
