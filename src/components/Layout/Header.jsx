44bab294503d6151b53c8a5fc9e84aae130b91d1
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-26 01:43:05 +0200

    Update Header Page

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,31 +5,38 @@ import Link from "next/link";
 function Header() {
   const session = useSession();
   console.log(session);
-  
+  const status = session.status;
     return (
       <header className="flex items-center justify-between mb-16">
-      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-      <Link href="/" className="text-primary font-semibold text-2xl">
-        ST PIZZA
-      </Link>
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link href="/" className="text-primary font-semibold text-2xl">
+            ST PIZZA
+          </Link>
           <Link href={"/"}>Home</Link>
           <Link href={""}>Menu</Link>
           <Link href={""}>About</Link>
           <Link href={""}>Contact</Link>
         </nav>
         <nav className="flex gap-4 items-center text-gray-500 font-semibold">
-          <Link
-            href={"/login"}
-            
-          >
-            Login
-          </Link>
-          <Link
-            href={"/register"}
-            className="bg-primary text-white px-8 py-2 rounded-full"
-          >
-            Register
-          </Link>
+          {status === "authenticated" && (
+            <button
+              onClick={() => signOut()}
+              className="bg-primary text-white px-8 py-2 rounded-full"
+            >
+              Logout
+            </button>
+          )}
+          {status === "unauthenticated" && (
+            <>
+              <Link href={"/login"}>Login</Link>
+              <Link
+                href={"/register"}
+                className="bg-primary text-white px-8 py-2 rounded-full"
+              >
+                Register
+              </Link>
+            </>
+          )}
         </nav>
       </header>
     );
