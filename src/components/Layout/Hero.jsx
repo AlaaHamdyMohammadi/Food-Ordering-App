bd3e5fb0c2762c8ff60a283079fbffde6221cb48
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-01-26 01:54:37 +0200

    Update Header Page

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -18,7 +18,7 @@ function Hero() {
             Order now
             <Right />
           </button>
-          <button className="flex items-center gap-2 text-gray-600 font-semibold">
+          <button className="flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold">
             Learn more
             <Right />
           </button>
